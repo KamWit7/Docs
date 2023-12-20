b79141a8cb89d5228243e53be6bcd1ee52b0936e
KamWit7 <kamilw77@interia.pl>
2023-12-20 15:46:11 +0100

    Add details component template and styles

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,19 +1,44 @@
 import { Component, inject } from "@angular/core"
 import { CommonModule } from "@angular/common"
 import { ActivatedRoute } from "@angular/router"
+import { HousingService } from "../housing.service"
+import { HousingLocation } from "../housing-location"
 
 @Component({
   selector: "app-details",
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>{{ housingLocationId }}</p> `,
-  styles: [],
+  template: ` <article>
+    <img [src]="housingLocation?.photo" alt="" className="listing-photo" />
+
+    <section className="listing-description">
+      <h2 className="listing-heading">{{ housingLocation?.name }}</h2>
+      <p className="listing-location">
+        {{ housingLocation?.city }}, {{ housingLocation?.state }}
+      </p>
+    </section>
+    <section className="listing-features">
+      <h2 className="section-heading">About this housing location</h2>
+      <ul>
+        <li>Unit available: {{ housingLocation?.availableUnits }}</li>
+        <li>Does this location have wifi {{ housingLocation?.wifi }}</li>
+        <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
+      </ul>
+    </section>
+    <section className="listing-apply">
+      <h2 className="section-heading">Apply now to live here</h2>
+      <button type="button" className="primary">Apply now</button>
+    </section>
+  </article>`,
+  styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute)
-  housingLocationId = 0
+  housingService: HousingService = inject(HousingService)
+  housingLocation: HousingLocation | undefined
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.paramMap.get("id"))
+    const id = Number(this.route.snapshot.paramMap.get("id"))
+    this.housingLocation = this.housingService.getHousingLocationById(id)
   }
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,33 +1,12 @@
-import { Component, inject } from "@angular/core"
-import { CommonModule } from "@angular/common"
-import { HousingLocationComponent } from "../housing-location/housing-location.component"
-import { HousingLocation } from "../housing-location"
-import { HousingService } from "../housing.service"
+import { Injectable } from "@angular/core"
+import { HousingLocation } from "./housing-location"
 
-@Component({
-  selector: "app-home",
-  standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
-  template: `
-    <section>
-      <form action="">
-        <input type="text" placeholder="Filter by city" />
-        <button type="submit" className="primary">Search</button>
-      </form>
-      <section className="results">
-        <app-housing-location
-          *ngFor="let housingLocation of housingLocationList"
-          [housingLocation]="housingLocation"
-        ></app-housing-location>
-      </section>
-    </section>
-  `,
-  styleUrl: "./home.component.css",
+@Injectable({
+  providedIn: "root",
 })
-export class HomeComponent {
+export class HousingService {
   readonly baseUrl = "https://angular.dev/assets/tutorials/common"
-
-  housingLocationList: HousingLocation[] = [
+  protected housingLocationList: HousingLocation[] = [
     {
       id: 0,
       name: "Acme Fresh Start Housing",
@@ -129,9 +108,12 @@ export class HomeComponent {
       laundry: true,
     },
   ]
-  housingService: HousingService = inject(HousingService)
-
-  constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations()
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList
+  }
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(
+      (housingLocation) => housingLocation.id === id
+    )
   }
 }

6d7921a8f961ca668184b7baff95cc9f04c69d42
KamWit7 <kamilw77@interia.pl>
2023-12-20 12:51:18 +0100

    Update housingLocation variable name in HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,7 +19,7 @@ import { HousingLocation } from "../housing-location"
       </form>
       <section className="results">
         <app-housing-location
-          *ngFor="let housingLocation of housingLocations"
+          *ngFor="let housingLocation of housingLocationList"
           [housingLocation]="housingLocation"
         ></app-housing-location>
       </section>
@@ -28,13 +28,15 @@ import { HousingLocation } from "../housing-location"
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
-  housingLocations: HousingLocation[] = [
+  readonly baseUrl = "https://angular.dev/assets/tutorials/common"
+
+  housingLocationList: HousingLocation[] = [
     {
       id: 0,
       name: "Acme Fresh Start Housing",
       city: "Chicago",
       state: "IL",
-      photo: "/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg",
+      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
       availableUnits: 4,
       wifi: true,
       laundry: true,
@@ -44,7 +46,7 @@ export class HomeComponent {
       name: "A113 Transitional Housing",
       city: "Santa Monica",
       state: "CA",
-      photo: "/assets/brandon-griggs-wR11KBaB86U-unsplash.jpg",
+      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
       availableUnits: 0,
       wifi: false,
       laundry: true,
@@ -54,7 +56,7 @@ export class HomeComponent {
       name: "Warm Beds Housing Support",
       city: "Juneau",
       state: "AK",
-      photo: "/assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg",
+      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
       availableUnits: 1,
       wifi: false,
       laundry: false,
@@ -64,7 +66,7 @@ export class HomeComponent {
       name: "Homesteady Housing",
       city: "Chicago",
       state: "IL",
-      photo: "/assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg",
+      photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
       availableUnits: 1,
       wifi: true,
       laundry: false,
@@ -74,7 +76,7 @@ export class HomeComponent {
       name: "Happy Homes Group",
       city: "Gary",
       state: "IN",
-      photo: "/assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg",
+      photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
       availableUnits: 1,
       wifi: true,
       laundry: false,
@@ -84,7 +86,7 @@ export class HomeComponent {
       name: "Hopeful Apartment Group",
       city: "Oakland",
       state: "CA",
-      photo: "/assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg",
+      photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
       availableUnits: 2,
       wifi: true,
       laundry: true,
@@ -94,7 +96,7 @@ export class HomeComponent {
       name: "Seriously Safe Towns",
       city: "Oakland",
       state: "CA",
-      photo: "/assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg",
+      photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
       availableUnits: 5,
       wifi: true,
       laundry: true,
@@ -104,7 +106,7 @@ export class HomeComponent {
       name: "Hopeful Housing Solutions",
       city: "Oakland",
       state: "CA",
-      photo: "/assets/r-architecture-GGupkreKwxA-unsplash.jpg",
+      photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
       availableUnits: 2,
       wifi: true,
       laundry: true,
@@ -114,7 +116,7 @@ export class HomeComponent {
       name: "Seriously Safe Towns",
       city: "Oakland",
       state: "CA",
-      photo: "/assets/saru-robert-9rP3mxf8qWI-unsplash.jpg",
+      photo: `${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg`,
       availableUnits: 10,
       wifi: false,
       laundry: false,
@@ -124,7 +126,7 @@ export class HomeComponent {
       name: "Capital Safe Towns",
       city: "Portland",
       state: "OR",
-      photo: "/assets/webaliser-_TPTXZd9mOo-unsplash.jpg",
+      photo: `${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg`,
       availableUnits: 6,
       wifi: true,
       laundry: true,
